fc174e5c26c67f2333d24fa3b33a3fdf91efde16
geek-o-geek <arjun.sisodia8@gmail.com>
2025-11-30 20:43:24 +0530

    google tag added

--- app/layout.jsx
+++ app/layout.jsx
@@ -47,6 +47,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-WLMHVJ9P');`
+          }}
+        />
+        {/* END Google Tag Manager */}
        <link rel="canonical" href="https://noidaluxhomes.vercel.app/" />
       </head>
       <body className="bg-[#fbfbfb] text-[#2c3e50] antialiased">
@@ -185,6 +196,18 @@ export default function RootLayout({ children }) {
               }),
             }}
           />
+
+          {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WLMHVJ9P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* END Google Tag Manager noscript */}
+
 
         {children}
       </body>
